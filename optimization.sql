79421b7fc8e6e49b5f307e635601082f98fdc8fa
mykhailoyershov25-lang <mykhailo.yershov.25@kse.org.ua>
2026-07-04 00:08:17 +0300

    fix

--- optimization.sql
+++ optimization.sql
@@ -1,7 +1,13 @@
-explain analyze select * from order_items where product_id = 3; -- Час запиту 38
+explain analyze 
+select * 
+from order_items 
+where product_id = 3; -- Час запиту 38
 
 
 create index idx_order_items_product_id on order_items(product_id);
 
 
-explain analyze select * from order_items where product_id = 3; -- Час запиту 20
+explain analyze 
+select * 
+from order_items 
+where product_id = 3; -- Час запиту 20
